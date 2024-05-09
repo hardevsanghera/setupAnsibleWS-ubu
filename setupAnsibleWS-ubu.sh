9bf4e303810ca3fe5ed9b67031238e25af881426
hardevsanghera <hardev_sanghera@yahoo.co.uk>
2024-05-09 19:12:20 +0100

    Just a single file for help with setup of a fresh Ubinti system

---
+++ setupAnsibleWS-ubu.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Setup an ansible Workstation on Ubuntu 24.04
+set -x
+
+#Some queries
+lsb_release -a
+cat /etc/os-rlease
+
+#Start installing software
+sudo apt-get update
+Sudo apt-get -y install open-vm-tools-desktop sshpass git opensshserver
+
+#xrdp
+wget https://www.c-nergy.be/downloads/xRDP/xrdp-installer-1.5.zip
+unzip xrdp-installer-1.5.zip 
+chmod +x  ~/Downloads/xrdp-installer-1.5.sh
+./xrdp-installer-1.5.sh
+#./xrdp-installer-1.5.sh -r     (to remove the xrdp packages)
+
+#Configure ssh server
+sudo cp /etc/ssh/ssh_config /etc/ssh_ssh_config_org
+#Uncomment #.  Passwordauthentication no to yes
+sudo sed -i "s/Passwordauthentication no/Passwordauthentication yes/"
+sudo systemctl start ssh
+sudo systemctl enable ssh
+
+#install ansible
+#https://www.youtube.com/watch?v=1LhV87kjHlo
+#Some queries
+apt-cache search ansible
+apt list ansible
+apt list available ansible
+apt install -y ansible
+ansible –version
+apt list available ansible-core
+apt upgrade ansible
+#cd to home folder of user (which allows sudo)
+# cd /etc/ansible
+#Nosuch file or directory
+sudo mkdir -pv /etc/ansible
+sudo cd /etc/ansible
+sudo ls
+sudo ansible-config init –disabled > ansible.cfg
+#vi hosts
+sudo bash -c "echo localhost ansible_connection=local >> ansible.cfg"
+ansible all -m ping
+
+#Python3
+cd ~
+sudo apt install -y python3 python3-pip
+
+sudo rm /usr/lib/python3.12/EXTERNaLLY-MANAGED
+#the .12 is version specific
+
+#setup for VMware
+pip install pyvmomi --user
+pip install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git --user
+ansible-galaxy collection install community.vmware
+
+#setup for Nutanix
+#https://www.nutanix.dev/2022/08/05/getting-started-with-the-nutanix-ansible-module/
+#1. Clone the GitHub repository to a local directory
+git clone https://github.com/nutanix/nutanix.ansible.git
+#2. Git checkout release version
+ver="v1.9.0"
+git checkout $ver -b $ver
+#3. Build the collection
+ansible-galaxy collection build
+#4. Install the collection
+#Note add --force option for rebuilding or reinstalling to overwrite existing data
+ansible-galaxy collection install nutanix-ncp-$ver.tar.gz --force
+
+#Install vscode
+echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" | sudo \ tee /etc/apt/sources.list.d/vs-code.list
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo apt-get update
+sudo apt-get install code
+#You will need to install your extensions
